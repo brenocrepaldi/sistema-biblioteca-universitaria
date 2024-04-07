d4e954f61424df24a5cf39bab3f8f948473335c5
Breno Gaia Crepaldi <brenogaia2004@gmail.com>
2024-04-07 16:09:53 -0300

    modificando estrutura do livro

--- modelo-perry-wolf/model/livro.py
+++ modelo-perry-wolf/model/livro.py
@@ -1,9 +1,9 @@
 class Livro:
-    def __init__(self, titulo, autor, ano, isbn):
+    def __init__(self, titulo, autor, ano, codigo):
         self.titulo = titulo
         self.autor = autor
         self.ano = ano
-        self.isbn = isbn
+        self.codigo = codigo
 
     def __str__(self):
-        return f"Livro: {self.titulo}, Autor: {self.autor}, Ano: {self.ano}, ISBN: {self.isbn}"
+        return f"Livro: {self.titulo}, Autor: {self.autor}, Ano: {self.ano}, Código: {self.codigo}"
